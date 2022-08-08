8638780ca45e22b34b1f74579313fea9dda2cad0
krista <krista@hirewinston.com>
2022-08-08 13:21:26 -0400

    Update Sup.py

--- pages/Sup.py
+++ pages/Sup.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write('Writing bios is not fun. But watching a very large neural network write you bio is kinda fun! That is why I created this app with the [cohere.ai](https://docs.cohere.ai/) API. Enjoy and [let me know your thoughts](https://twitter.com/thekristalee)')
+st.write('Writing bios is not fun. But watching a very large neural network write you bio is kinda fun! That is why I created this app with the [cohere.ai](https://docs.cohere.ai/) API. Enjoy and feel free to [send me your thoughts](https://twitter.com/thekristalee)')
