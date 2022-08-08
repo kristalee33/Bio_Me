a417dbf333d2452f03d335e8770a1c07633f6619
krista <krista@hirewinston.com>
2022-08-08 13:16:11 -0400

    Create about.py

---
+++ pages/about.py
@@ -0,0 +1,1 @@
+st.write('Writing bios is not fun. But watching a very large neural network write you bio is kinda fun! That is why I created this app witht he cohere.ai API. Enjoy and let me know your thoughts. https://twitter.com/thekristalee')
