426b623f6e219fd3b72b2dafde97cdb497f6d9ce
krista <krista@hirewinston.com>
2022-08-08 13:35:49 -0400

    Update Sup.py

--- pages/Sup.py
+++ pages/Sup.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write('Writing bios is cringey. But watching a very large neural network to write bios is amusing, at least for me :). That is why I created this app with the [cohere.ai](https://docs.cohere.ai/) API. Feel free to send [me](https://twitter.com/thekristalee)feedback.')
+st.write('Writing bios is cringey. But watching a very large neural network to write bios is amusing, at least for me :). That is why I created this app with the [cohere.ai](https://docs.cohere.ai/) API. Feel free to send [me](https://twitter.com/thekristalee) feedback.')
